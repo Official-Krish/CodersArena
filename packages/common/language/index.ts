d687797549458406dde7fa379e2830a2f4beacaa
Official-Krish <krishanand0103@gmail.com>
2024-06-16 18:15:54 +0530

    Added language in seed data

--- packages/common/language/index.ts
+++ packages/common/language/index.ts
@@ -8,5 +8,6 @@ export const LANGUAGE_MAPPING: {
   } = {
     js: { judge0: 63, internal: 1, name: "Javascript", monaco: "javascript" },
     cpp: { judge0: 54, internal: 2, name: "C++", monaco: "cpp" },
-    rs: { judge0: 73, internal: 3, name: "python", monaco: "python" },
+    rs: { judge0: 73, internal: 3, name: "Rust", monaco: "rs" },
+    py: { judge0: 56, internal: 4, name: "Python", monaco: "python" },
   };
