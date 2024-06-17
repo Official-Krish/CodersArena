7d3a1dc5e1e6a440cc4a9db9d4346ce24abd71a4
Official-Krish <krishanand0103@gmail.com>
2024-06-17 20:47:48 +0530

    updated appbar

--- apps/web/components/Appbar.tsx
+++ apps/web/components/Appbar.tsx
@@ -1,14 +1,15 @@
 "use client";
 import { signIn, useSession } from "next-auth/react";
 import Avatar from "./Avatar"
+import Link from "next/link";
 
 export const Appbar = () => {
     const { data: session } = useSession();
     return (
         <div className="flex justify-between py-4 px-8 bg-gray-900 text-white">
-            <div>
+            <Link href ="/">
                 Coders Arena
-            </div>
+            </Link >
             <div className="flex justify-between mr-7"> 
                 <div className="mr-8">
                     Contests
