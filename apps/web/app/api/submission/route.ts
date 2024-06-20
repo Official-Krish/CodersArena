93250f776219b77bf728dccde08fbadb5e890f87
Official-Krish <krishanand0103@gmail.com>
2024-06-20 21:35:39 +0530

    added route for submit

--- apps/web/app/api/submission/route.ts
+++ apps/web/app/api/submission/route.ts
@@ -2,6 +2,12 @@ import { getServerSession } from "next-auth";
 import { NextRequest, NextResponse } from "next/server";
 import { authOptions } from "../../lib/auth";
 import { db } from "../../db";
+import { SubmissionInput } from "@repo/common/zod";
+import { getProblem } from "../../lib/problems";
+import axios from "axios";
+import { LANGUAGE_MAPPING } from "@repo/common/language";
+
+const JUDGE0_URL = process.env.JUDGE0_URL
 
 export async function GET(req: NextRequest) {
     const session = await getServerSession(authOptions);
@@ -56,4 +62,93 @@ export async function GET(req: NextRequest) {
             status: 200,
         }
     );
+}
+
+export async function POST(req: NextRequest) {
+    const session = await getServerSession(authOptions);
+    if (!session?.user) {
+        return NextResponse.json({
+                message: "You must be logged in to submit a problem",
+            },
+            {
+                status: 401,
+            }
+        );
+    }
+    const userId = session.user.id;
+    // add rate limiting
+
+    const submissionInput = SubmissionInput.safeParse(await req.json());
+    if (!submissionInput.success) {
+        return NextResponse.json({
+            message: "Invalid submission input",
+        },
+        {
+            status: 400,
+        });
+    }
+
+    let formData = new FormData();
+    formData.append("response", submissionInput.data.token);
+
+    const dbProblem = await db.problem.findUnique({
+        where: {
+            id: submissionInput.data.problemId,
+        },
+    });
+
+    if (!dbProblem) {
+        return NextResponse.json({
+            message: "Problem not found",
+        },
+        {
+            status: 404,
+        });
+    }
+
+    const problem = await getProblem(
+        dbProblem.slug,
+        submissionInput.data.languageId
+    )
+    problem.fullBoilerplateCode = problem.fullBoilerplateCode.replace(
+        "##USER_CODE_HERE##",
+        submissionInput.data.code
+    );
+
+    const response = await axios.post(
+        `${JUDGE0_URL}/submissions/batch?base64_encoded=false`,
+        {
+            submissions : problem.inputs.map(((input, index)=>{
+                language_id: LANGUAGE_MAPPING[submissionInput.data.languageId]?.judge0,
+                source_code: problem.fullBoilerplateCode.replace(
+                    "#INPUT_FILE_INDEX#",
+                    index.toString()
+                ),
+                expected_output: problem.outputs[index],
+            })),
+        }
+    );
+
+    const submission = await db.submission.create({
+        data: {
+            userId: session.user.id,
+            problemId: dbProblem.id,
+            code : submissionInput.data.code,
+            activeContestId: submissionInput.data.activeContestId,
+            testcases:{
+                connect: response.data,
+            },
+        },
+        include: {
+            testcases: true,
+        }
+    })
+
+    return NextResponse.json({
+        message : "Submission made",
+        id : submission.id,
+    },
+    {
+        status: 200,
+    });
 }
